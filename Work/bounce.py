ea06019326342427d01fcc36c7e74e21c8b0e16d
Kryttax <kryttax@gmail.com>
2020-11-10 16:49:20 +0100

    Exercise 1.11

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+i = 0
+while i < 10:
+    meters = (height * 3) / 5
+    height = meters
+    i = i + 1
+    
+    print(i, ' ', round(meters, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000.0
+normal_payment = 2684.11
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    if(extra_payment_start_month < months 
+    and months < extra_payment_end_month):
+        payment = normal_payment + extra_payment
+    else:
+        payment = normal_payment
+
+    if principal < payment:
+        payment = principal
+        
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months = months + 1
+
+    print(months, total_paid, principal)
+
+print('Total paid:', total_paid)
+print('Months:', months)
