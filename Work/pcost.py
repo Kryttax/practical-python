4201bff201eaf4c6a00efcff4ac21441b2ca27a3
Kryttax <kryttax@gmail.com>
2020-11-12 16:53:44 +0100

    Finished Containers & Formatting

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import sys
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    totalCost = 0.0
+    try:
+        with open(filename, 'rt') as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+
+            for row in rows:
+                totalCost += float(row[2]) * int(row[1])   
+        return totalCost
+    except ValueError:
+        raise RuntimeError('Invalid file path. Please check you entered a correct path.')
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+    
+print(f"Total cost: {portfolio_cost(filename)}")
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,73 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+portfolio = []
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {
+                'name': row[0],
+                'shares': int(row[1]), 
+                'price': float(row[2])
+            }
+
+            #holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+        
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if row:
+               prices[row[0]] = float(row[1])
+
+    return prices    
+        
+
+def gain_loss(portfolio, prices):
+    total_cost = 0.0
+    total_value = 0.0
+
+    for s in portfolio:
+        total_cost += s['shares']*s['price']
+
+    for s in portfolio:
+        total_value += s['shares']*prices[s['name']]
+
+    return total_value - total_cost
+
+def single_gain_loss(share, price, current_price):
+    total_cost = 0.0
+    total_value = 0.0
+
+    total_cost += share*price
+
+    total_value += share*current_price
+
+    return total_value - total_cost
+
+def make_report(portfolio, prices):
+    report = []
+    for s in portfolio:
+        #report.append((s[0], int(s[1]), float(prices[s[0]]), single_gain_lossint(int(s[1]),float(s[2]),float(prices[s[0]]))))
+        
+        sname = s['name']
+        sshare = s['shares']
+        sprice = prices[s['name']]
+        schange = sprice - s['price']
+        #report.append((s['name'], s['shares']))
+        report.append((sname, sshare, sprice, schange))
+    return report
+         
