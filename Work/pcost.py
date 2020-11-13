796b7340a51b44f4dcef7e44dae7b01d2654869e
Kryttax <kryttax@gmail.com>
2020-11-13 15:19:00 +0100

    New lesson group finished. -- Working with Program Structure.

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,18 +3,30 @@
 # Exercise 1.27
 
 import sys
+import csv
 
 def portfolio_cost(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
-    totalCost = 0.0
+    total_cost = 0.0
+
     try:
         with open(filename, 'rt') as f:
             rows = csv.reader(f)
             headers = next(rows)
 
-            for row in rows:
-                totalCost += float(row[2]) * int(row[1])   
-        return totalCost
+            for rowno, row in enumerate(rows, start=1):
+                record = dict(zip(headers, row))
+                
+                try:
+                    nshares = int(record['shares'])
+                    price = float(record['price'])
+                    total_cost += nshares * price
+                    # This catches errors in int() and float() conversions above
+                except ValueError:
+                    print(f'Row {rowno}: Bad row: {row}')
+        
+        return total_cost
+
     except ValueError:
         raise RuntimeError('Invalid file path. Please check you entered a correct path.')
 
